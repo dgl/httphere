582b0cd23d7f9678f66b1fe81a77e0a891e41919
David Leadbeater <dgl@dgl.cx>
2014-05-06 23:51:22 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,40 @@
+HTTP Here
+=========
+
+Very simple golang webserver to serve the current directory. For when you just
+want a web server, here, now.
+
+    $ httphere
+    2014/05/06 23:17:47 Opening server on :8080 available at:
+      - http://127.0.0.1:8080/
+      - http://192.0.2.36:8080/
+      - http://[::1]:8080/
+      - http://[2001:db8:11a9:c:cafe:4eee:eeee:eee]:8080/
+      - http://[fe80::5e51:4eee:eeee:eee]:8080/
+
+If you trust me (and I have a build for your system):
+
+    curl -O https://dgl.cx/httphere/$(uname -sm | tr 'A-Z ' 'a-z-')/httphere
+    chmod +x httphere
+    ./httphere
+
+(This will serve the current directory, doing this in your home directory may be
+a bad idea. Better to store it in your ~/bin)
+
+Why?
+====
+
+I was using the simple:
+
+    python3 -m http.server
+
+However this is single threaded and will block if you have a download in
+progress. Also it didn't support IPv6 by default. Both of which are a bit lame.
+So this is a very simple go program.
+
+Building
+========
+
+   go build -ldflags -s httphere.go
+
+Using -ldflags -s cuts the executable size in half.
---
+++ httphere.go
@@ -0,0 +1,105 @@
+// Runs a HTTP server in the current directory.
+// David Leadbeater, 2014. http://dgl.cx/licence (WTFPL).
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/user"
+)
+
+var (
+	quiet *bool
+	port  *string
+	safe  *bool
+)
+
+func init() {
+	var default_port string
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal("Cannot determine uid: ", err)
+	}
+	if u.Uid == "0" {
+		default_port = ":http"
+	} else {
+		default_port = ":8080" // http-alt, but not all systems know this
+	}
+	quiet = flag.Bool("quiet", false, "Don't log except for errors")
+	port = flag.String("port", default_port, "[ip]:port to listen on")
+	safe = flag.Bool("safe", true, "Only serve world readable files. "+
+		"This is merely a safety measure to avoid accidents, do not rely on it.")
+	flag.Parse()
+}
+
+func main() {
+	http.Handle("/", http.FileServer(dirWrapper("")))
+	if !*quiet {
+		log.Print("Opening server on ", *port, " available at:")
+		printAddrs()
+	}
+	err := http.ListenAndServe(*port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printAddrs() {
+	ip, port, err := net.SplitHostPort(*port)
+	if err != nil {
+		log.Print("Unable to split host/port: ", err)
+		return
+	}
+	if ip == "" {
+		interfaces, err := net.InterfaceAddrs()
+		if err == nil {
+			for _, addr := range interfaces {
+				printAddr(addr.(*net.IPNet).IP.String(), port)
+			}
+		} else {
+			log.Print("Unable to query interfaces: ", err)
+		}
+	} else {
+		printAddr(ip, port)
+	}
+}
+
+func printAddr(addr, port string) {
+	if port == "http" {
+		port = "80"
+	}
+	fmt.Printf("  - http://%v/\n", net.JoinHostPort(addr, port))
+}
+
+// Wrap a http.Dir with a very basic permission check. Beware, this only checks
+// one level (not if it can recurse into the directory it is inside) this is
+// designed to avoid accidents like serving your ssh private key or something.
+type dirWrapper string
+func (d dirWrapper) Open(path string) (http.File, error) {
+	if !*quiet {
+		log.Print(path)
+	}
+	file := path
+	// Canonicalise, for some reason we get a request for "//index.html"
+	for len(file) > 0 && file[0] == '/' {
+		file = file[1:]
+	}
+	if file == "" {
+		file = "."
+	}
+	fi, err := os.Stat(file)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	if *safe && fi.Mode().Perm()&4 == 0 {
+		log.Print(d, path, " is not world readable")
+		return nil, errors.New("Not world readable")
+	}
+	return http.Dir(d).Open(path)
+}
